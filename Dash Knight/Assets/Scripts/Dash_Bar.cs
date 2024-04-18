6f7ee3fa1d5396251ef5efdbb16edb6eaf9b9abe
harrisebenjamin <harrisebenjamin@gmail.com>
2024-04-17 22:49:28 -0500

    Update Dash_Bar.cs

--- Dash Knight/Assets/Scripts/Dash_Bar.cs
+++ Dash Knight/Assets/Scripts/Dash_Bar.cs
@@ -9,6 +9,7 @@ public class Dash_Bar : MonoBehaviour
 
     public static Dash_Bar instance;
 
+    //Sets up the dash bar singleton
     private void Awake()
     {
         instance = this;
@@ -28,16 +29,19 @@ public class Dash_Bar : MonoBehaviour
         
     }
 
+    //Sets the slider value to 0
     public void Dash()
     {
         slider.value = slider.minValue;
     }
 
+    //Starts the coroutine to increase the slider value by 1 each second
     public void Recharge()
     {
         StartCoroutine(Count());
     }
 
+    //Increases the slider value by 1 each second
     private IEnumerator Count()
     {   
         while(slider.value < slider.maxValue)
